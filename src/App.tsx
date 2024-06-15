4a708896860789987f353a41e0a86188e552badd
cmZhou <332877152@qq.com>
2024-06-15 23:30:14 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
-            </div >
+            </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
